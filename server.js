8fc04e727c147b0c02cbc6cb80270835af64d0a9
KarlitosD <carlosdcampos.04@gmail.com>
2022-10-17 01:40:09 -0300

    try to fix root folder in production to bundle module

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import { renderToString } from "https://esm.sh/vue@3.2.41/server-renderer";
 import { createApp } from "./src/main.js";
 
 
-import { bundle } from "https://deno.land/x/emit@0.9.0/mod.ts";
-const { code } = await bundle(new URL("./client.js", import.meta.url))
+import { bundle } from "https://deno.land/x/emit@0.9.0/mod.ts"
+const { code } = await bundle("./client.js")
+
+console.log({ code })
 
 export const renderApp = async () => {
     const app = createApp()
